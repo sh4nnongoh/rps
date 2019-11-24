2f747c0ed2a548c431f614ad64d3f41f4c15443a
sh4nnongoh <sh4nnongoh@gmail.com>
2019-11-24 14:38:31 +0800

    Working Build For Custom React

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    module: {
+        rules: [{
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
